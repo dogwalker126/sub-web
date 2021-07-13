511715b205f1d07f331e6eada7d603faf7aad084
dogwalker126 <61008110+dogwalker126@users.noreply.github.com>
2021-07-13 12:22:01 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -257,19 +257,12 @@ export default {
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
           "shadowingy-sub.vercel.app(shadowingy提供-稳定)": "https://shadowingy-sub.vercel.app/sub?",
-          "subcon.dlj.tf(subconverter作者提供-稳定)":
-            "https://subcon.dlj.tf/sub?",
-          "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
-          "api.wcc.best(sub-web作者提供-稳定)": "https://api.wcc.best/sub?",
-          "sub.id9.cc(品云转换)": "https://sub.id9.cc/sub?",
+
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
           { value: "https://shadowingy-sub.vercel.app/sub?" },
-          { value: "https://subcon.dlj.tf/sub?" },
-          { value: "https://api.dler.io/sub?" },
-          { value: "https://api.wcc.best/sub?" },
-          { value: "https://sub.id9.cc/sub?" },
+
         ],
         remoteConfig: [
           {
