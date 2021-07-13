da8ac92eed58dd03692923b8c06ab09684e3b65d
dogwalker126 <61008110+dogwalker126@users.noreply.github.com>
2021-07-13 11:19:34 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -4,7 +4,7 @@
       <el-col>
         <el-card>
           <div slot="header">
-            Shadowingy官网-订阅转换-
+            Shadowingy官网-订阅转换-shadowingypro.xyz
             <svg-icon icon-class="github" style="margin-left: 20px" @click="goToProject" />
             <svg-icon icon-class="telegram" style="margin-left: 20px" @click="gotoTgChannel" />
 
@@ -543,7 +543,7 @@ export default {
   },
   created() {
     // document.title = "Subscription Converter";
-    document.title = "在线订阅转换";
+    document.title = "Shadowingy官网-在线订阅转换";
      this.isPC = this.$getOS().isPc;
 
     // 获取 url cache
